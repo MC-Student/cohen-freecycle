2c13e4b71b39e6abdb7eec969f5fa6fdf893b240
MC-Student <meiratova123@gmail.com>
2023-05-16 21:37:12 -0400

    Added test for controller, test passes

---
+++ src/test/java/cohen/mvc/FreebiePostsControllerTest.java
@@ -0,0 +1,40 @@
+package cohen.mvc;
+
+import cohen.FreebieService;
+import cohen.json.PostListInfo;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class FreebiePostsControllerTest
+{
+    static
+    {
+        RxJavaPlugins.setIoSchedulerHandler(scheduler -> Schedulers.trampoline());
+        RxJavaPlugins.setNewThreadSchedulerHandler(scheduler -> Schedulers.trampoline());
+    }
+
+    @Test
+    void refreshPosts()
+    {
+        //given
+        FreebieService service = mock();
+        JTextArea postList = mock();
+        FreebiePostsController controller = new FreebiePostsController(service, postList);
+        PostListInfo postListInfo = mock();
+        Observable<PostListInfo> observableList = Observable.just(postListInfo);
+        doReturn(observableList).when(service).getPostList();
+
+        //when
+        controller.refreshPosts();
+
+        //then
+        verify(service).getPostList();
+    }
+}
